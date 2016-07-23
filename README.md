e758d1433f8f9c6fcfbc29d14d694dec3e647a01
ironstein1994 <ironstein1994@gmail.com>
2016-07-23 17:36:55 +0530

    added properties in float.py to enable modifying the Float object after it is created

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-IEEE 754 - 2008 compliant single precision floating point multiplier architecture for FPGA devices.
+IEEE 754 - 2008 compliant single precision floating point multiplier
+architecture for FPGA devices.
+
+**Dependencies** :
+Kivy : can be downloaded from [here](https://kivy.org/docs/gettingstarted/installation.html) (only required for gui related functionality)
+Note : while installing kivy, there might be a problem if you do not have "pygame" installed on your system. If that is the case, install pygame from [here](http://www.pygame.org/download.shtml)
--- project/playing_with_floating_point_numbers/src/float.py
+++ project/playing_with_floating_point_numbers/src/float.py
@@ -2,6 +2,10 @@
 from bitstring import bitstring as bs
 
 class Float(bs.BitArray) :
+	'''
+	Extends the bitstring.BitArray class by implementing 
+	floating point number specific methods 
+	'''
 
 	def __init__(self, num=0, length=32) :
 		pass
@@ -12,6 +16,32 @@ class Float(bs.BitArray) :
 		f = super(Float, cls).__new__(cls, float=num, length=length)
 		return f
 
+	# value
 	@property
 	def value(self):
 		return self.float
+
+	@value.setter
+	def value(self, num) : 
+		x = super(bs.BitArray, self).__new__(self.__class__, float=num, length=self.len)
+		self._datastore = x._datastore
+
+	# raw_mantissa
+	@property
+	def raw_mantissa(self):
+		if self.len == 32 : 
+			return self.bin[9:]
+		return self.bin[11:]
+
+	# raw_exponent
+	@property
+	def raw_exponent(self):
+		if self.len == 32 : 
+			return self.bin[1:9]
+		return self.bin[1:12]
+
+	# raw_sign
+	@property 
+	def raw_sign(self) : 
+		return self.bin[0]
+	
